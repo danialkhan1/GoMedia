8299d59fff0173010cb16663148476765b0f2cfe
MSI\DK <danial.khan@ucl.ac.uk>
2019-03-11 10:43:56 +0000

    attempt at function promises - to be removed

---
+++ data_utils.js
@@ -0,0 +1,21 @@
+let getUrl = new promise(function(resolve, reject) {
+  var obj = {
+      referenceUrl: 'https://github.com/webpack/webpack/tree/af0164',
+      url: 'https://github.com/webpack/webpack'
+  }
+
+
+});
+
+
+export function getUrl(){
+
+}
+
+  var obj = {
+    name:  name,
+    totalScore: totalScore,
+    gamesPlayed: gamesPlayed
+  };
+  // Return it
+  return obj;
