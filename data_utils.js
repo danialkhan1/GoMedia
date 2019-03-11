125fef101d1429b14f8ff2b0d354805b44a01c79
MSI\DK <danial.khan@ucl.ac.uk>
2019-03-11 10:47:19 +0000

    removed function promise and replaced with regular function for getUrl

--- data_utils.js
+++ data_utils.js
@@ -1,21 +1,12 @@
-let getUrl = new promise(function(resolve, reject) {
+function getUrl() {
   var obj = {
       referenceUrl: 'https://github.com/webpack/webpack/tree/af0164',
       url: 'https://github.com/webpack/webpack'
-  }
-
-
-});
+  };
+  return obj;
+}
 
 
 export function getUrl(){
 
 }
-
-  var obj = {
-    name:  name,
-    totalScore: totalScore,
-    gamesPlayed: gamesPlayed
-  };
-  // Return it
-  return obj;
